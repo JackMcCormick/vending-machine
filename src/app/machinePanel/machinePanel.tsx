bf9c5967789b7a390da9ce8de3f2620662970a2d
Jack McCormick <jackbmccormick93@gmail.com>
2021-08-28 11:59:26 -0400

    common input component

--- src/app/machinePanel/machinePanel.tsx
+++ src/app/machinePanel/machinePanel.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { GSInput } from "../../ui-kit/GS-input";
 import { vendingContext } from "../vending-context";
 
 interface machinePanelProps {}
@@ -7,9 +8,24 @@ export function MachinePanel(props: machinePanelProps) {
   const { vendingMachineState, updateVendingMachineState } =
     useContext(vendingContext);
 
+  function handleOnChangeTest(event: any) {}
+
+  function handleOnBlurTest(event: any) {}
+
   return (
     <>
-      <div>Home Page Test</div>
+      <div className={"container"}>
+        <GSInput
+          id={"idTest"}
+          name={"nameTest"}
+          label={"LabelTest"}
+          onChange={handleOnChangeTest}
+          onBlur={handleOnBlurTest}
+          isInvalid={true}
+          value={"Test"}
+          validationMessage={"Test Validation Message"}
+        />
+      </div>
     </>
   );
 }
